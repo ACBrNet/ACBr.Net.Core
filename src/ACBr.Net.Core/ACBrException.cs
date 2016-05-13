22e69de04b476a08b01f4be0e4fdf0a545534e7d
batiati <rbatiati@gmail.com>
2016-05-13 13:18:00 -0300

    Inclusão da classe ACBrException

---
+++ src/ACBr.Net.Core/ACBrException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ACBr.Net.Core
+{
+	public class ACBrException : ApplicationException
+	{
+		#region Constructor
+
+		public ACBrException(string message) : base(message)
+		{
+		}
+
+		public ACBrException(string format, params object[] args) : base(string.Format(format, args))
+		{
+		}
+
+		#endregion Constructor
+	}
+}
