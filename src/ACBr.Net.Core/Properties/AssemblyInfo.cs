aaccf1cdaf5b9a197a4e072404ba079edfd7f7fd
RFTD <rfteno@gmail.com>
2016-11-24 10:58:25 -0300

    [*] Removendo configurações de COM Interop.

--- src/ACBr.Net.Core/Properties/AssemblyInfo.cs
+++ src/ACBr.Net.Core/Properties/AssemblyInfo.cs
@@ -4,17 +4,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-#if COM_INTEROP
-[assembly: AssemblyTitle("ACBr.Net Core ActiveX")]
-[assembly: AssemblyDescription("ACBr.Net Core ActiveX")]
-[assembly: AssemblyProduct("ACBr.Net Core ActiveX")]
-[assembly: TypeLibVersion(109, 23)]
-#else
 [assembly: AssemblyTitle("ACBr.Net Core")]
 [assembly: AssemblyDescription("ACBr.Net Core")]
 [assembly: AssemblyProduct("ACBr.Net Core")]
-#endif
-
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ACBr.Net")]
 [assembly: AssemblyCopyright("Copyright © Grupo ACBr.Net 2014 - 2016")]
@@ -23,11 +15,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-#if COM_INTEROP
-[assembly: ComVisible(true)]
-#else
 [assembly: ComVisible(false)]
-#endif
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("72CEC839-4E63-4AEA-A2B4-370ED6F44B1A")]
@@ -42,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.4")]
 [assembly: AssemblyFileVersion("1.0.1.4")]
 
 //Internal Visible
